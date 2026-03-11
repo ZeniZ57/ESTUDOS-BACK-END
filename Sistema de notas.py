91759d62dba492bc652bea2187f7360d78e7b2e0
Walisson Moreira <walissonmoreira180@gmail.com>
2026-03-11 18:13:40 -0300

    Enhance comments for better code clarity
    Added comments to clarify the purpose of each section of the code.

--- Sistema de notas.py
+++ Sistema de notas.py
@@ -4,17 +4,22 @@ continuar = "s"
 while continuar == "s":
 
  media = float(input("digite a nota do aluno:"))
+ #o usuario vai digitar a nota do aluno
 
  if media >= 7:
     print("aluno aprovado!!")
+  #se a nota for maior ou igual a 7, o aluno é aprovado
 
  elif media >= 6:
     print("aluno de recuperaçao!!")
-
+  #se o aluno tirar 6, ficou na recuperaçao
  else:
     print("aluno reprovado!!")
+  #aqui se a nota for baixa, reprovaçao
     continuar = input("quer calcular a média de outro aluno? (s/n)")
-
+  #o programa pergunta ao usuario se quer continuar a calcular as notas
 print("obrigado por usar o sistema de notas!")
+#essa mensagem aparece quando o usuario aperta N
     
+
 
